9ba1e1436b5eded0f2a379d567b40e2bdda84629
HTThanhf <savecloud406@gmail.com>
2025-12-17 18:49:51 +0700

    Add Firebase config

--- firebase.js
+++ firebase.js
@@ -1,7 +1,25 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { getFirestore, collection, addDoc, getDocs, onSnapshot } from 'firebase/firestore';
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  doc,
+  getDoc,
+  updateDoc,
+  arrayUnion
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// === DÁN CONFIG CỦA M Ở ĐÂY ===
 const firebaseConfig = {
   apiKey: "AIzaSyAiLtwLXqewgfygF8_A3thHtf-2Gnjheqo",
   authDomain: "dungeon-mayhem-game.firebaseapp.com",
@@ -12,8 +30,24 @@ const firebaseConfig = {
   measurementId: "G-YWCKPNKL3P"
 };
 
+// Khởi tạo
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, collection, addDoc, getDocs, onSnapshot };
+export {
+  auth,
+  db,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  doc,
+  getDoc,
+  updateDoc,
+  arrayUnion
+};
